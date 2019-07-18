c3b6e2b5f5b02dcdaf0463596cf2f7c392172e53
christianesk <christian.91.gm@hotmail.com>
2019-07-18 17:09:15 -0500

    Input, Output ViewChild y Graficas con ng2-Chart

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+//ng2-charts
+
+import { ChartsModule } from 'ng2-charts';
+
+//temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 
 
 
@@ -20,7 +28,9 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -30,7 +40,8 @@ import { Graficas1Component } from './graficas1/graficas1.component';
     imports:[
         SharedModule,
         PAGES_ROUTES,
-        FormsModule
+        FormsModule,
+        ChartsModule
     ]
 })
 
